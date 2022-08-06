669d5c086da90f826facb67b172b2f3a3958d6a3
Lucas Pyaia <lucaspyaia@hotmail.com>
2022-08-06 17:03:15 -0300

    Exercise 3.4

---
+++ 3 - Stacks and Queues/StacksQueues/3_4/MyQueue.cs
@@ -0,0 +1,41 @@
+﻿namespace _3_4
+{
+    public class MyQueue<T>
+    {
+        private readonly Stack<T> _stack1;
+
+        private readonly Stack<T> _stack2;
+
+        public MyQueue()
+        {
+            _stack1 = new Stack<T>();
+            _stack2 = new Stack<T>();
+        }
+
+        public void Queue(T value)
+        {
+            while (!_stack2.IsEmpty())
+            {
+                _stack1.Push(_stack2.Pop());
+            }
+
+            Console.WriteLine($"Queueing: {value}");
+
+            _stack1.Push(value);
+        }
+
+        public T Unqueue()
+        {
+            while (!_stack1.IsEmpty())
+            {
+                _stack2.Push(_stack1.Pop());
+            }
+
+            T value = _stack2.Pop();
+
+            Console.WriteLine($"Unqueing: {value}");
+
+            return value;
+        }
+    }
+}
---
+++ 3 - Stacks and Queues/StacksQueues/3_4/Program.cs
@@ -0,0 +1,17 @@
+﻿using _3_4;
+
+var queue = new MyQueue<int>();
+
+queue.Queue(1);
+queue.Queue(2);
+queue.Queue(3);
+queue.Queue(4);
+
+queue.Unqueue();
+
+queue.Queue(5);
+
+queue.Unqueue();
+queue.Unqueue();
+queue.Unqueue();
+queue.Unqueue();
