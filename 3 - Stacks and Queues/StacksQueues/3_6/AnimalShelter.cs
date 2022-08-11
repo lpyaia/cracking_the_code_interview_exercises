80ce5585f3a703dee4971e37d3a979f22d0f6680
Lucas Pyaia <lucaspyaia@hotmail.com>
2022-08-10 23:19:44 -0300

    Exercise 3.6

---
+++ 3 - Stacks and Queues/StacksQueues/3_6/AnimalShelter.cs
@@ -0,0 +1,96 @@
+﻿namespace _3_6
+{
+    public class AnimalShelter
+    {
+        private readonly LinkedList<Animal> _animals;
+
+        public AnimalShelter()
+        {
+            _animals = new LinkedList<Animal>();
+        }
+
+        public void ArriveAnimal(Animal animal)
+        {
+            if (_animals.Last == null)
+            {
+                _animals.AddFirst(animal);
+            }
+
+            else
+            {
+                var current = _animals.First;
+
+                while (current != null && animal.DateOfArrival >= current.Value.DateOfArrival)
+                {
+                    current = current.Next;
+                }
+
+                if (current != null)
+                {
+                    _animals.AddBefore(current, animal);
+                }
+
+                else
+                {
+                    _animals.AddLast(animal);
+                }
+            }
+        }
+
+        public Animal? DequeueDog()
+        {
+            return DequeueAnimal(Animal.Type.Dog);
+        }
+
+        public Animal? DequeueCat()
+        {
+            return DequeueAnimal(Animal.Type.Cat);
+        }
+
+        public Animal? DequeueAny()
+        {
+            return _animals.First?.Value;
+        }
+
+        private Animal? DequeueAnimal(Animal.Type typeAnimal)
+        {
+            Animal? dog = null;
+            LinkedListNode<Animal>? current = _animals.First;
+
+            while (current != null && current.Value.TypeAnimal != typeAnimal)
+            {
+                current = current.Next;
+            }
+
+            if (current != null)
+            {
+                dog = current.Value;
+                _animals.Remove(current);
+            }
+
+            return dog;
+        }
+    }
+
+    public record Animal
+    {
+        public string Name { get; set; }
+
+        public DateTime DateOfArrival { get; set; }
+
+        public Type TypeAnimal { get; set; }
+
+        public Animal(string name, DateTime dateOfArrival, Type typeAnimal)
+        {
+            Name = name;
+            DateOfArrival = dateOfArrival;
+            TypeAnimal = typeAnimal;
+        }
+
+        public enum Type
+        {
+            Dog = 0,
+            Cat = 1
+        }
+    }
+}
---
+++ 3 - Stacks and Queues/StacksQueues/3_6/Program.cs
@@ -0,0 +1,45 @@
+﻿using _3_6;
+
+var animalShelter = new AnimalShelter();
+
+animalShelter.ArriveAnimal(new Animal("2022-05-10", new DateTime(2022, 5, 10), Animal.Type.Dog));
+animalShelter.ArriveAnimal(new Animal("2022-05-11", new DateTime(2022, 5, 11), Animal.Type.Dog));
+animalShelter.ArriveAnimal(new Animal("2020-01-01", new DateTime(2020, 1, 1), Animal.Type.Dog));
+animalShelter.ArriveAnimal(new Animal("2021-02-02", new DateTime(2021, 1, 2), Animal.Type.Dog));
+animalShelter.ArriveAnimal(new Animal("2022-06-01", new DateTime(2022, 6, 1), Animal.Type.Dog));
+animalShelter.ArriveAnimal(new Animal("2019-12-01", new DateTime(2019, 12, 1), Animal.Type.Dog));
+
+animalShelter.ArriveAnimal(new Animal("2012-05-10", new DateTime(2012, 5, 10), Animal.Type.Cat));
+animalShelter.ArriveAnimal(new Animal("2012-05-11", new DateTime(2012, 5, 11), Animal.Type.Cat));
+animalShelter.ArriveAnimal(new Animal("2010-01-01", new DateTime(2010, 1, 1), Animal.Type.Cat));
+animalShelter.ArriveAnimal(new Animal("2011-02-02", new DateTime(2011, 1, 2), Animal.Type.Cat));
+animalShelter.ArriveAnimal(new Animal("2012-06-01", new DateTime(2012, 6, 1), Animal.Type.Cat));
+animalShelter.ArriveAnimal(new Animal("2009-12-01", new DateTime(2009, 12, 1), Animal.Type.Cat));
+
+Console.WriteLine(animalShelter.DequeueDog()!.Name);
+Console.WriteLine(animalShelter.DequeueDog()!.Name);
+Console.WriteLine(animalShelter.DequeueDog()!.Name);
+Console.WriteLine(animalShelter.DequeueDog()!.Name);
+Console.WriteLine(animalShelter.DequeueDog()!.Name);
+Console.WriteLine(animalShelter.DequeueDog()!.Name);
+Console.WriteLine("------");
+Console.WriteLine(animalShelter.DequeueCat()!.Name);
+Console.WriteLine(animalShelter.DequeueCat()!.Name);
+Console.WriteLine(animalShelter.DequeueCat()!.Name);
+Console.WriteLine(animalShelter.DequeueCat()!.Name);
+Console.WriteLine(animalShelter.DequeueCat()!.Name);
+Console.WriteLine(animalShelter.DequeueCat()!.Name);
+Console.WriteLine("------");
+animalShelter.ArriveAnimal(new Animal("2022-05-10", new DateTime(2022, 5, 10), Animal.Type.Dog));
+animalShelter.ArriveAnimal(new Animal("2022-05-11", new DateTime(2022, 5, 11), Animal.Type.Dog));
+animalShelter.ArriveAnimal(new Animal("2020-01-01", new DateTime(2020, 1, 1), Animal.Type.Dog));
+animalShelter.ArriveAnimal(new Animal("2021-02-02", new DateTime(2021, 1, 2), Animal.Type.Dog));
+animalShelter.ArriveAnimal(new Animal("2022-06-01", new DateTime(2022, 6, 1), Animal.Type.Dog));
+animalShelter.ArriveAnimal(new Animal("2019-12-01", new DateTime(2019, 12, 1), Animal.Type.Dog));
+animalShelter.ArriveAnimal(new Animal("2012-05-10", new DateTime(2012, 5, 10), Animal.Type.Cat));
+animalShelter.ArriveAnimal(new Animal("2012-05-11", new DateTime(2012, 5, 11), Animal.Type.Cat));
+animalShelter.ArriveAnimal(new Animal("2010-01-01", new DateTime(2010, 1, 1), Animal.Type.Cat));
+animalShelter.ArriveAnimal(new Animal("2011-02-02", new DateTime(2011, 1, 2), Animal.Type.Cat));
+animalShelter.ArriveAnimal(new Animal("2012-06-01", new DateTime(2012, 6, 1), Animal.Type.Cat));
+animalShelter.ArriveAnimal(new Animal("2009-12-01", new DateTime(2009, 12, 1), Animal.Type.Cat));
+Console.WriteLine(animalShelter.DequeueAny()!.Name);
